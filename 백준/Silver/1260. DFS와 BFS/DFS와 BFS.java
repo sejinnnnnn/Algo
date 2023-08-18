5cc9b70256a2e917ee871df9a13f2fc1a8f3ea81
세 진 <50567363+sejinnnnnn@users.noreply.github.com>
2023-08-18 17:07:44 +0900

    [Silver II] Title: DFS와 BFS, Time: 356 ms, Memory: 20712 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,18 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 1260. DFS와 BFS
+ * 기본적인 DFS와 BFS를 구현하는 문제임
+ * 2달전에 파이썬으로 풀었음에도 불구하고 자바로 푸는 데 조금 틀렸다
+ * 일단 정점들을 sort했어야 했기 때문에 이중 LinkedList로 인접리스트를 구현하였다.
+ * 메서드에서 정점 하나 방문할 때 마다 정답 문자열에 하나씩 추가해주었다 !
+ * 메모리 : 21080KB, 시간 : 416ms
+ * @author SSAFY
+ *
+ */
 public class Main {
 	
 	static int n;
 	static int m;
-	static LinkedList<LinkedList<Integer>> adjList;
+	static ArrayList<ArrayList<Integer>> adjList;
 	static boolean[] visited;
 	
 	static StringBuilder answer;
@@ -28,11 +38,11 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 		int start = Integer.parseInt(st.nextToken());
 		
-		adjList = new LinkedList<>();
+		adjList = new ArrayList<>();
 		visited = new boolean[n + 1];
 		
 		for (int i = 0; i <= n; i++) {
-			adjList.add(new LinkedList<>());
+			adjList.add(new ArrayList<>());
 		}
 		
 		for (int i = 0; i < m; i++) {
