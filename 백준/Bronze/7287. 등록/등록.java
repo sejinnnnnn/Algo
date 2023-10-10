b653a0fd95102d9037d5485c3717a758a3de0b49
세 진 <50567363+sejinnnnnn@users.noreply.github.com>
2023-10-10 15:19:30 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11192 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("164\nsejinnnnnn");
+		
+	}
+
+}
