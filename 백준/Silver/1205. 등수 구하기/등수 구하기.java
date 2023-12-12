9f14047ae5631288a12294494e70d5b4dee486aa
세 진 <50567363+sejinnnnnn@users.noreply.github.com>
2023-12-12 16:09:10 +0900

    [Silver IV] Title: 등수 구하기, Time: 80 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -2,6 +2,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 1205. 등수 구하기
+ * 처음엔 PriorityQueue 사용해서 풀다가 .. 예외처리가 많아지면서 코드가 복잡해지고 무엇보다 정답이 50% 대에서 자꾸 틀렸다
+ * 그 후 값을 확인하면서 범위를 벗어나는지, 포함되는지를 체크하고 포함된다면 동률을 고려해서 순위를 출력했더니 정답 !
+ *
+ * 메모리 : 11,536 KB, 시간 : 84 ms
+ */
 public class Main {
 
     public static void main(String[] args) throws Exception {
