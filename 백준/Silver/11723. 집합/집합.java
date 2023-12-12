f4a9515362dc5bbece75092dd82842c50fd14cd3
세 진 <50567363+sejinnnnnn@users.noreply.github.com>
2023-12-12 12:54:27 +0900

    [Silver V] Title: 집합, Time: 1412 ms, Memory: 325348 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
 
-    static TreeSet<Integer> set;
+    static HashSet<Integer> set;
     static StringBuilder answer;
 
     static final List<Integer> allList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
@@ -17,7 +14,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         answer = new StringBuilder();
 
-        set = new TreeSet<>();
+        set = new HashSet<>();
 
         StringTokenizer st = null;
 
