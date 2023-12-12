c812e582bbe796e8e764eab2550f1818a7bbdd8e
세 진 <50567363+sejinnnnnn@users.noreply.github.com>
2023-12-12 13:05:48 +0900

    [Silver V] Title: 집합, Time: 988 ms, Memory: 324708 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 /**
  * 백준 11723. 집합
- * Java 내장클래스인 HashSet을 활용하여 각 명령어에 맞는 메소드 호출하였음
- *
- * 메모리 : 325,348 ms, 시간 : 1,412 ms
+ * 처음에는 Java 내장클래스인 TreeSet, HashSet을 활용하여 각 명령어에 맞는 메소드 호출하였으나, 시간이 구데기로 나옴 (2380, 1412)
+ * 집합 요소의 범위를 체크해보니 1~20 이여서, 20 크기의 boolean 배열로 관리할 수 있겠다고 생각해서 바꿨음
+ * 메모리 : 325,084 ms, 시간 : 992 ms
  */
 public class Main {
 
@@ -51,14 +51,21 @@ public class Main {
 
     }
 
+    /**
+     * 집합에 현재 값이 있는지 없는지 확인하고, 정답 문자열에 0 또는 1 추가
+     * @param x 확인할 값
+     */
     private static void check(int x) {
         if (isIn[x]) answer.append("1\n");
         else answer.append("0\n");
     }
 
+    /**
+     * 주어진 값이 있으면 지우고, 없으면 추가하기
+     * @param x toggle 할 값
+     */
     private static void toggle(int x) {
-        if (isIn[x]) isIn[x] = false;
-        else isIn[x] = true;
+        isIn[x] = !isIn[x];
     }
 
 }
